c0bc1be71e96b162b68e6ee6712b157c8a703a23
trOOnies <facundomartinscasso@gmail.com>
2024-10-29 00:19:40 -0300

    implementing addons and offering

--- dbdie_classes/options/IMPLEMENTED.py
+++ dbdie_classes/options/IMPLEMENTED.py
@@ -4,10 +4,14 @@ from dbdie_classes.options import KILLER_FMT
 from dbdie_classes.options import SURV_FMT
 
 FMTS = [
+    KILLER_FMT.ADDONS,
+    SURV_FMT.ADDONS,
     KILLER_FMT.CHARACTER,
     SURV_FMT.CHARACTER,
     KILLER_FMT.ITEM,
     SURV_FMT.ITEM,
+    KILLER_FMT.OFFERING,
+    SURV_FMT.OFFERING,
     KILLER_FMT.PERKS,
     SURV_FMT.PERKS,
     SURV_FMT.STATUS,
